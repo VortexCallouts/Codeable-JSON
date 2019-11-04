a07de99b44f8df795a42fdcb9bb0b2cf5f7e9668
VortexCallouts <ftvortex1@gmail.com>
2019-11-04 13:27:30 -0600

    Uploaded Project

---
+++ Codable and JSON/PhotoSetViewController.swift
@@ -0,0 +1,32 @@
+
+
+import UIKit
+
+class PhotoSetViewController: UIViewController {
+    @IBOutlet weak var displayTextView: UITextView!
+    
+    let jsonFileName = "photos"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let photoSet = PhotoSetLoader.load(jsonFileName: jsonFileName) {
+            var displayInfo = ""
+            displayInfo += "Status: \(photoSet.status)\n"
+            displayInfo += "Photos path: \(photoSet.photosPath)\n\n"
+            for photo in photoSet.photos {
+                displayInfo += "Photo:\n"
+                displayInfo += "\(photo.title)\n"
+                displayInfo += "\(photo.description)\n"
+                displayInfo += "\(photo.date)\n"
+                displayInfo += "\(photo.latitude)\n"
+                displayInfo += "\(photo.longitude)\n"
+                displayInfo += "\(photo.imageName)\n\n"
+            }
+            displayTextView.text = displayInfo
+        } else {
+            displayTextView.text = "Error."
+        }
+    }
+}
+
